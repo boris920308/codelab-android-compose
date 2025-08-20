9861cc5a95a7486aa3528cc81da38f3c5f913542
boris920308 <jhoonj0308@gmail.com>
2025-08-21 00:38:34 +0900

    feat. dynamic Theme 제거

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/theme/Theme.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/theme/Theme.kt
@@ -256,25 +256,18 @@ val unspecified_scheme = ColorFamily(
 
 @Composable
 fun AppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-  val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-          val context = LocalContext.current
-          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      
-      darkTheme -> darkScheme
-      else -> lightScheme
-  }
+    val colors = if (!useDarkTheme) {
+        lightScheme
+    } else {
+        darkScheme
+    }
 
-  MaterialTheme(
-    colorScheme = colorScheme,
-    typography = AppTypography,
-    content = content
-  )
+    MaterialTheme(
+        colorScheme = colors,
+        content = content
+    )
 }
 
